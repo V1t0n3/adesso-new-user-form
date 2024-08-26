2b4d42feb6fe03b8927917384620dd23a8b0280b
Vito Chiaradia <vitochiaradia@gmail.com>
2024-08-26 21:33:07 +0200

    add user form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css'
+import LoginPage from './pages/LoginPage';
 
 function App() {
 
   return (
     <>
+    <div className="root">
+    <LoginPage />
+    </div>
     </>
-  )
+  );
 }
 
 export default App
